08c67cf10dc8c189331f14a893e1e15b646be266
Ephanon <codeparadoxist@gmail.com>
2025-05-03 14:48:53 +0900

    [Chore] 별찍기 - 1

--- BOJ_Cpp/2438.cpp
+++ BOJ_Cpp/2438.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
@@ -14,6 +15,6 @@ int main() {
 			if (j <= i)
 				cout << "*";
 		}
-		cout << endl;
+		cout << '\n';
 	}
 }
