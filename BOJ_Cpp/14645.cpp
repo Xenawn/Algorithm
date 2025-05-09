b0b4a32f00e4ec60638c6f419eab66bc98dd14d4
Ephanon <codeparadoxist@gmail.com>
2025-05-09 17:02:28 +0900

    [Update] 와이버스 부릉부릉

---
+++ BOJ_Cpp/14645.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	int n, k, a, b;
+
+	cin >> n >> k;
+
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+	}
+
+	cout << "ºñ¿ÍÀÌ";
+}
