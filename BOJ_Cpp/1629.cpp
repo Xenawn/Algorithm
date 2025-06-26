e40bec36bf6b5367cfd8cf7c99e1f6a67a7dc47b
Ephanon <codeparadoxist@gmail.com>
2025-06-27 00:16:33 +0900

    [Update] 곱셈

--- BOJ_Cpp/1629.cpp
+++ BOJ_Cpp/1629.cpp
@@ -6,14 +6,15 @@ long long multiply(long long a, long long b, long long m) {
 	if (b == 1) return a % m;
 	long long val = multiply(a, b / 2, m);
 	val = val * val % m;
-	if (b == 0) 
+	if (b%2 == 0) 
 		return val;
 	return val * a % m;
 }
 
 int main() {
-
-	int a, b, c;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	long long a, b, c;
 	cin >> a >> b >> c;
 
 	cout<<multiply(a, b, c);
