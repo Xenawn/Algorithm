9b0e838092f3344c6601907b9131573be1952983
Ephanon <codeparadoxist@gmail.com>
2025-07-03 23:33:41 +0900

    [Update] 오븐 시계

---
+++ BOJ_Cpp/2525.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	int h = c / 60;
+	int m = b + c;
+	h = m / 60;
+	m %= 60;
+	if (a + h < 24) {
+		cout << a + h << ' ' << m;
+	}
+	else  {
+		cout << (a + h) % 24 << ' ' << m;
+	}
+}
