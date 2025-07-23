2bf5c452199bd7990d2cead2702dd115fd2f7270
Ephanon <codeparadoxist@gmail.com>
2025-07-23 14:33:22 +0900

    [Update] 수 정렬하기 5

---
+++ BOJ_Cpp/15688.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int freq[2000001];
+int main() {
+
+	// Counting Sort
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	int a;
+
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		freq[num + 1000000]++;
+	}
+
+	for (int i = 0; i < 2000000; i++) {
+
+		while (freq[i]--) cout << i-1000000 << '\n';
+	}
+}
