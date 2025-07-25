8af0e2332094741cc608b975996c326bbf2589d7
Ephanon <codeparadoxist@gmail.com>
2025-07-25 13:10:37 +0900

    [Update[ 시리얼 번호_수정

--- BOJ_Cpp/1431.cpp
+++ BOJ_Cpp/1431.cpp
@@ -4,56 +4,49 @@ using namespace std;
 // 시리얼 번호
 // 생각해야할 것 숫자 vs 영어만
 bool cmp(string str1, string str2) {
-    if (str1.length() != str2.length()) { // 길이가 같을 때
-        if (str1.length() < str2.length()) return true;
-        return false;
-    }
-    else { // 길이가 같지 않을 때
-        int sum1 = 0; // 숫자만
-        int sum2 = 0; // 숫자만
-        for (char a : str1) {
-            if (a >= '0' && a <= '9') {  // 숫자만 더함
-                sum1 += (a - '0');
-            }
-        }
+	if (str1.length() != str2.length()) { // 길이가 같을 때
+		if (str1.length() < str2.length()) return true;
+		return false;
+	}
+	else { // 길이가 같지 않을 때
+		int sum1 = 0; // 숫자만
+		int sum2 = 0; // 숫자만
+		for (char a : str1) {// 숫자만 더함
+			if (a >= '0' && a <= '9') {
+				sum1 += (a - '0');
+			}
+		}
 
-        for (char b : str2) {  // 숫자만 더함
-            if (b >= '0' && b <= '9') {
-                sum2 += (b - '0');
-            }
-        }
+		for (char b : str2) {  // 숫자만 더함
+			if (b >= '0' && b <= '9') {
+				sum2 += (b - '0');
+			}
+		}
 
-        if ((sum1 != 0 || sum2 != 0)) { //  0이 아닐 때
-            if (sum1 == sum2) { // 합이 같을 때
-                return str1 < str2;  // 사전 순
-            }
-            else
-                return sum1 < sum2;
-        }
-        else { // 둘다 0일 때
-            return str1 < str2;
-        }
-
-    }
-    return false;
+		if (sum1 == sum2)  // 합이 같을 때
+			return str1 < str2;  // 사전 순
+		else
+			return sum1 < sum2;
+	}
+	return str1 < str2;
 }
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-    int n;
-    vector<string> v;
-    cin >> n;
+	int n;
+	vector<string> v;
+	cin >> n;
 
-    for (int i = 0; i < n; i++) {
-        string str;
-        cin >> str;
-        v.push_back(str);
-    }
+	for (int i = 0; i < n; i++) {
+		string str;
+		cin >> str;
+		v.push_back(str);
+	}
 
-    sort(v.begin(), v.end(), cmp);
+	sort(v.begin(), v.end(), cmp);
 
-    for (int i = 0; i < n; i++) {
-        cout << v[i] << '\n';
-    }
+	for (int i = 0; i < n; i++) {
+		cout << v[i] << '\n';
+	}
 }
