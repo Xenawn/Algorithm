b3e2e7527faf1194657976e95d579595b9174fb4
Ephanon <codeparadoxist@gmail.com>
2025-08-21 16:35:05 +0900

    [Update] 정수 삼각형

---
+++ BOJ_Cpp/1932.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int arr[502][502];
+int d[502][502];
+int main() {
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j <= i; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	d[0][0] = arr[0][0];
+	int choice = 0;
+	for (int i = 1; i <= n; i++) {
+
+		for (int j = 0; j <= i; j++) {
+			if (j == 0) d[i][j] = d[i - 1][j] + arr[i][j];
+			else if (i == j) d[i][j] = d[i - 1][j - 1] + arr[i][j];
+			else {
+				d[i][j] = max(d[i - 1][j - 1], d[i - 1][j]) + arr[i][j];
+			}
+		}
+	
+	}
+
+	cout << *max_element(d[n], d[n] + n);
+}
