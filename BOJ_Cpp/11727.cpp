533d2bdbf63b0e943555f085220ef6bf962d7bed
Ephanon <codeparadoxist@gmail.com>
2025-08-22 09:28:11 +0900

    [Update] 2×n 타일링 2

---
+++ BOJ_Cpp/11727.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int d[1002];
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	d[1] = 1;
+	d[2] = 3;
+	cin >> n;
+
+	for (int i = 3; i <= n; i++) {
+		d[i] = (d[i - 2] +d[i-2]+d[i-1]) % 10007;
+	}
+
+	cout << d[n];
+}
