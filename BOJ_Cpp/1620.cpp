9c43cd4d2ee9a7349baf936595f6c09601f3d12b
Ephanon <codeparadoxist@gmail.com>
2025-09-01 22:16:22 +0900

    [Update] 나는야 포켓몬 마스터 이다솜

---
+++ BOJ_Cpp/1620.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+unordered_map<string,int> s;
+string arr[100001];
+int main() {
+	
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n, m;
+	
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		string str;
+		cin >> str;
+		s[str] = i + 1;
+		arr[i + 1] = str;
+	}
+	
+	for (int i = 0; i < m; i++) {
+		string str;
+		cin >> str;
+		if (isdigit(str[0])) {
+			int num = stoi(str);
+			cout << arr[num] << '\n';
+		}
+		else
+			cout << s[str] << '\n';
+	}
+}
