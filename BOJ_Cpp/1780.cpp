1769870b52e3559ed044d57fcda2940ef9c802b6
Ephanon <codeparadoxist@gmail.com>
2025-10-10 17:44:30 +0900

    [Update] 종이의 개수

--- BOJ_Cpp/1780.cpp
+++ BOJ_Cpp/1780.cpp
@@ -1,55 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int arr[2200][2200];
+int arr[2188][2188];
 int cnt[3];
-bool compare(int a, int b, int n) {
+int n;
 
-	for (int i = a; i < a+n; i++) {
-		for (int j = b; j <b+ n; j++) {
-			if(arr[i][j]!=arr[a][b])
-			return false;
+bool check(int x, int y, int n) {
+
+	for (int i = x; i < x +n; i++) {
+		for (int j = y; j < y + n; j++) {
+			if (arr[i][j] != arr[x][y])
+			{
+				return false;
+			}
 		}
+
+		
 	}
 	return true;
 }
-void func(int a, int b, int n) {
 
+void solve(int x, int y, int z) {
 	
-	if (compare(a,b,n)){
-		cnt[arr[a][b]+1] += 1;
+	if (check(x, y, z)) {
+		cnt[arr[x][y] + 1] += 1;
 		return;
 	}
-	
+
+	int n = z / 3;
+
 	for (int i = 0; i < 3; i++) {
+
 		for (int j = 0; j < 3; j++) {
-			
-			
-				func(a+i*n, b+i*n, n / 3);
-			
+			solve(x+i*n, y+j*n, n);
 		}
 	}
-	
-
-	
 }
 int main() {
 
-	int n;
-	int count = 0;
 	cin >> n;
-	
-
 
 	for (int i = 0; i < n; i++) {
-
 		for (int j = 0; j < n; j++) {
 			cin >> arr[i][j];
 		}
 	}
-
-	func(0, 0, n);
-
+	solve(0, 0, n);
 	for (int i = 0; i < 3; i++) {
 		cout << cnt[i] << endl;
 	}
