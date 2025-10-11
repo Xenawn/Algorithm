5e21c5443a83007b75414896d8d3f3bdc754ac7f
Ephanon <codeparadoxist@gmail.com>
2025-10-11 17:06:49 +0900

    [Update] 카드 게임

---
+++ BOJ_Cpp/5522.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	int sum = 0;
+
+	int num = 0;
+
+	for (int i = 0; i < 5; i++) {
+		cin >> num;
+		sum += num;
+	}
+
+	cout << sum;
+	
+}
