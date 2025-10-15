bdfc11ed4a188ee2f5944af3f80cab56cfde9a3c
Ephanon <codeparadoxist@gmail.com>
2025-10-15 21:41:21 +0900

    N과 M (2)

---
+++ BOJ_Cpp/15650.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+bool isUsed[10];
+int arr[10];
+void func(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+	}
+	int st = 1;
+	if (k != 0) st = arr[k - 1] + 1;
+	for (int i = st; i <= n; i++) {
+		arr[k] = i;
+		func(k + 1);
+		
+	}
+	
+}
+
+int main() {
+	cin >> n >> m;
+
+	func(0);
+}
