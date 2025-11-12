05ab7f8dac54e39c80ad6ae4b29b3eebc6aeadc7
Ephanon <codeparadoxist@gmail.com>
2025-11-12 21:46:27 +0900

    숫자 카드

---
+++ BOJ_Cpp/10815.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> v;
+int main() {
+	
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		v.push_back(num);
+	}
+
+	sort(v.begin(), v.end());
+
+	cin >> m;
+	for (int i = 0; i < m; i++) {
+		int num;
+		cin >> num;
+		
+		cout << binary_search(v.begin(), v.end(), num) << '\n';
+	}
+}
