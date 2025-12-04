fbf616d13d9c6fbd4c2566017e4637f60ecf4707
Xenawn <codeparadoxist@gmail.com>
2025-12-05 02:37:37 +0900

    카드 정렬하기

---
+++ BOJ_Cpp/1715.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	priority_queue<int,vector<int>,greater<int>> q;
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		q.push(x);
+	}
+	int sum = 0;
+
+	while (q.size() > 1) {
+		int a = q.top();
+		q.pop();
+		int b = q.top();
+		q.pop();
+		sum += a + b;
+		q.push(a + b);
+	}
+	cout << sum;
+}
