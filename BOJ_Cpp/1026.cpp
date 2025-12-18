19c8b40e986f220d9200c334f3106a0b29a61200
Xenawn <codeparadoxist@gmail.com>
2025-12-18 23:18:57 +0900

    보물

---
+++ BOJ_Cpp/1026.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[51];
+int b[51];
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+
+	cin >> n;
+
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	for (int i = 0; i < n; i++) {
+		cin >> b[i];
+	}
+
+	sort(a, a + n);
+	sort(b, b + n);
+	for (int i = 0; i < n; i++) {
+		sum += (a[i] * b[n - i - 1]);
+	}
+
+	cout << sum;
+}
